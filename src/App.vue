2b37cc1fde60c44fecd3dd1c4178d3c3e6916646
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-09-23 22:40:31 -0400

    Implement home page markup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view />
+  <div class="background">
+    <router-view />
+  </div>
 </template>
+
+<style scoped>
+.background {
+  background-image: url('./assets/img/morning_photo.jpg');
+  @apply bg-cover w-full h-screen;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="home"></div>
+  <div class="flex items-center justify-center h-full home">
+    <div class="px-2 py-3 sm:p-6 w-450 bg-gray-translucid">
+      <img class="m-auto" src="@/assets/img/logo.svg" alt="Forecaster" />
+
+      <hr class="w-full mt-4 border-gray-500 sm:mt-5" />
+
+      <div class="flex w-full mt-4 sm:mt-5">
+        <input
+          type="text"
+          class="flex-1 p-2 text-lg placeholder-gray-600 bg-white border border-gray-500 opacity-100"
+          placeholder="Where are you now?"
+        />
+        <button class="btn" disabled>
+          Go
+        </button>
+      </div>
+
+      <hr class="w-full mt-4 border-gray-500 sm:mt-5" />
+      <p class="mt-3 font-thin text-blue-900 sm:mt-4">
+        Designed and developed by <a href="#" class="link">Maxsuel Maccari</a>.
+        You can check all credits of used assets
+        <a href="#" class="link">here</a>.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,3 +34,21 @@ export default defineComponent({
   components: {},
 })
 </script>
+
+<style scoped>
+.btn {
+  @apply text-lg font-bold text-blue-900 bg-gray-500 px-2 ml-2;
+
+  &:disabled {
+    @apply text-gray-700 bg-gray-500 cursor-default;
+  }
+}
+
+.link {
+  @apply underline opacity-75;
+
+  &:hover {
+    @apply opacity-100;
+  }
+}
+</style>
