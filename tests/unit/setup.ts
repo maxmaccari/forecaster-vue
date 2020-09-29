e2c0f5f67e8a41f16162ba235da74deae262992d
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-09-29 19:42:01 -0400

    Fix vscode jest runner

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,3 +1,8 @@
 import { config } from '@vue/test-utils'
 
+// For vscode runner
+process.env.NODE_ENV='test'
+process.env.VUE_APP_API_KEY='FAKE_API_KEY'
+process.env.VUE_APP_API_URL='https://api-url.com'
+
 config.global.stubs = ['v-icon']
